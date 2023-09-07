ad44dab7e0635fc91336d805f2db23f80ecc24af
Justinperry999 <>
2023-09-07 16:16:17 +0000

    Add menu function and while loop for user to choose an option

--- run.py
+++ run.py
@@ -1,2 +1,25 @@
-import random import randint
+import random
 
+def menu():
+    print("[1] Instructions")
+    print("[2] Play Battleships")
+    print("[0] Exit Game")
+
+menu()
+option = int(input("Enter your option:\n"))
+
+while option != 0:
+    if option == 1:
+        #go to instructions
+        print("Instructions has been called")
+    elif option == 2:
+        #Play Battleships
+        print("Play Battleships has been called")
+    else:
+        print("Invalid option!")
+
+    print()
+    menu()
+    option = int(input("Enter your option:\n"))
+
+print("Thank you for playing Battleships! Goodbye!")

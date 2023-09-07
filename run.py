d4d1a3517c2c9aeb3e2950b32b0da962f057c010
Justinperry999 <>
2023-09-07 16:40:52 +0000

    Add create_board function

--- run.py
+++ run.py
@@ -1,6 +1,20 @@
 import random
 
+def create_board(board):
+    hidden_board = [[' '] * 8 for x in range[8]]
+    guess_board = [[' '] * 8 for x in range[8]]
+    letters_to_numbers = {"A":0,"B":1,"C":2,"D":3,"E":4,"F":5,"G":6,"H":7}
+    print('     A B C D E F G H')
+    print('     ---------------')
+    row_number = 1
+    for row in board:
+        print("%d|%s|" % (row_number, "|".join(row)))
+        row_number += 1
+
 def menu():
+    """
+    Allows user to choose whether to read instructions, play game or exit.
+    """
     print("[1] Instructions")
     print("[2] Play Battleships")
     print("[0] Exit Game")
@@ -23,3 +37,6 @@ while option != 0:
     option = int(input("Enter your option:\n"))
 
 print("Thank you for playing Battleships! Goodbye!")
+
+
+

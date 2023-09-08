3ff647cf1ccca7c94694f86a0834a91522e8bd62
Justinperry999 <>
2023-09-08 17:54:23 +0000

    create would you like to play menu and write code to create ships

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-from random import randint
+import random
 
 
 def menu():
@@ -26,57 +26,40 @@ while option != 2:
     menu()
     option = int(input("Enter your option:\n"))
 
-cpu_board = []
-user_board = []
+board = []
 
 for x in range(6):
-  cpu_board.append(["_"] * 6)
-  user_board.append(["_"] * 6)
+  board.append(["_"] * 6)
 
-def cpu_game_board(cpu_board):
-  for row in cpu_board:
-    print((" ").join(row))
-
-def user_game_board(user_board):
-  for row in user_board:
+def game_board(board):
+  for row in board:
     print((" ").join(row))
 
 print("Welcome to the Battleships!")
 print(" ")
-print("Computer's Board")
-cpu_game_board(cpu_board)
-print(" ")
-print("Your Board")
-user_game_board(user_board)
+print("Lets sink some ships!")
+game_board(board)
+
+print()
+play_game = input("Would you like to play? (y/n):\n")
+
+while play_game != "y":
+    if play_game == "n":
+        print("Thank you for playing Battleships! Goodbye!")
+        menu()
+    else:
+        print("Invalid option!")
+
+print("Initializing game...")
+
+def create_ships(board):
+    return random.randint(0, 6), random.randint(0, 6)
 
 
-
-
-
-
-
-
-
-
-"""
-def create_board(board):
-    hidden_board = [[' '] * 6 for x in range[6]]
-    guess_board = [[' '] * 6 for x in range[6]]
-    letters_to_numbers = {"A":0,"B":1,"C":2,"D":3,"E":4,"F":5}
-    print('     A B C D E F')
-    print('     -----------')
-    row_number = 1
-    for row in board:
-        print("%d|%s|" % (row_number, "|".join(row)))
-        row_number += 1
-
-def create_cpu_ships(board):
-    ship_row = randint(0,len(board)-1)
-    ship_col = randint(0, len(board[0])-1)
-    ship_position = ship_row,ship_col
-
-def rungame():
-    print("Welcome to Battleships!")    
-    create_board(board)
-    create_cpu_ships(board)
-"""
+ship1 = create_ships(board)
+ship2 = create_ships(board)
+ship3 = create_ships(board)
+ship4 = create_ships(board)
+ship5 = create_ships(board)
+ships_left = 5
+ammo = 15
